9b48b4e42cc0dc6d730bbcf840ac2fdbbc21c95b
miyazoe.daisuke <hndfavodaismi@gmail.com>
2021-12-04 10:45:24 +0900

    修正

--- hello/urls.py
+++ hello/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from . import views
+from .views import HelloView
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('next', views.next, name='next'),
+    path(r'', HelloView.as_view(), name='index'),
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
+from .forms import HelloForm
 
-def index(request):
-    params = {
-        'title': 'Hello/Index',
-        'msg': 'これは、サンプルで作ったページです。',
-        'goto':'next',
-    }
-    return render(request, 'hello/index.html', params)
+class HelloView(TemplateView):
+    
+    def __init__(self):
+        self.params = {
+            'title': 'Hello',
+            'form': HelloForm(),
+            'result':None
+        }
+    
+    def get(self, request):
+        return render(request, 'hello/index.html', self.params)
 
-def next(request):
-    params = {
-        'title': 'Hello/Index',
-        'msg': 'これは、サンプルで作ったページです。',
-        'goto':'next',
-    }
-    return render(request, 'hello/index.html', params)
+    def post(self, request):
+        ch = request.POST.getlist('choice')
+        self.params['result'] = 'selected: ' + str(ch) + '.'
+        self.params['form'] = HelloForm(request.POST)
+        return render(request, 'hello/index.html', self.params)
