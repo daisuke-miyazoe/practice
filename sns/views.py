f6fe14c5ca90782fd94391d7c8afee96c2fdeaa1
miyazoe.daisuke <hndfavodaismi@gmail.com>
2021-12-30 23:21:41 +0900

    修正

--- sns/views.py
+++ sns/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.core.paginator import Paginator
+from django.db.models import Q
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .models import Message, Friend, Group, Good
+from .forms import GroupCheckForm, GroupSelectForm, \
+    FriendsForm, CreateGroupForm, PostForm
+
+# indexのビュー関数
+@login_required(login_url='/admin/login/')
+def index(request, page=1):
+  # publicのuserを取得
+  (public_user, public_group) = get_public()
+
+  # POST送信時の
